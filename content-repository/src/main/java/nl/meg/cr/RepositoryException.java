553ae15e7ad1e8fc41dd221e538beb92cf632fc7
Michiel Eggermont <michiel.eggermont@gmail.com>
2015-12-30 14:03:38 +0100

    Remove dependencies on jcr-api

--- content-repository/src/main/java/nl/meg/cr/RepositoryException.java
+++ content-repository/src/main/java/nl/meg/cr/RepositoryException.java
@@ -1,7 +1,8 @@
 package nl.meg.cr;
 
 public class RepositoryException extends RuntimeException {
-    public RepositoryException(javax.jcr.RepositoryException e) {
+
+    public RepositoryException(Exception e) {
         super(e);
     }
 }
